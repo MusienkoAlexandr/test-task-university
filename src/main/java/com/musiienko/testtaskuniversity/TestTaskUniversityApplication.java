3058798168ed3acf8b2d08de3ea6ade543a9a28f
MusienkoAlexandr <alexandr.musienko1999@gmail.com>
2022-01-10 14:37:20 +0200

    Initial commit

---
+++ src/main/java/com/musiienko/testtaskuniversity/TestTaskUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.musiienko.testtaskuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskUniversityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskUniversityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musiienko/testtaskuniversity/TestTaskUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musiienko.testtaskuniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskUniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
